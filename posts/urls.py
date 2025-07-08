d8189d062875eac17af18a76a449c80a0bcfbd72
Ashlee Hodor <ashlee.hodor@SDGKU.edu>
2025-07-07 19:56:08 -0700

    django update

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,6 @@ from posts import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"), 
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"), 
+    path("new/", views.PostCreateView.as_view(), name="new"), 
 ]
